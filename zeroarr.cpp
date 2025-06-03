bb34b066f30456548a94681ebac571faf65c7be7
Aarav Arudia <aaravarudi@gmail.com>
2025-06-03 23:37:36 +0530

    correct

--- zeroarr.cpp
+++ zeroarr.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main()
 {
-
+  int a;
+  cin >> a;
+  int sum = 0;
+  int maxsum = 0;
+  for (int i = 0; i < a;i++)
+  {
+    int x;
+    cin >> x;
+    sum += x;
+    maxsum = max(x, maxsum);
+  }
+  if(sum%2==0 && maxsum<=sum-maxsum)
+    cout << "YES";
+    else
+      cout << "NO";
 }
